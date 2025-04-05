608003db3b1ef1e653472ade8bba38c88709f9f4
Edgar <edgar_pecero@hotmail.com>
2025-04-05 14:43:47 -0600

    Push commit

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,6 +1,7 @@
 import { EntityTypeEnum } from '@/types/BaseEntity';
 
 // const API_BASE_URL = 'http://localhost:5000/api';
+
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const defaultHeaders = {
